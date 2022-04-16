08380897d574c5e4a34e05c02921303ea138f9b1
geekhall <174821300@qq.com>
2022-04-16 10:11:04 +0800

    chore: init

---
+++ src/pages/plum.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue?: boolean
+}>()
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>Plum Blossom.</h1>
+    </div>
+  </div>
+</template>
